d1af531ddb0ba799ddba52c906a61529d59ba80b
alanraso <alanrraso@gmail.com>
2018-04-25 20:59:39 -0300

    soma e transf. em segundos

---
+++ Exercicios/Aula1/segundos.py
@@ -0,0 +1,6 @@
+# Dado um número inteiro de horas, imprimir quantos segundos se passam naquelas horas
+
+horas = int(input('Digite o número de horas: '))
+segundos = horas*60*60
+
+print(segundos, 'segundos')
---
+++ Exercicios/Aula1/soma.py
@@ -0,0 +1,7 @@
+# Dados dois números como entrada, calcular a soma deles
+
+a = int(input('Primeiro número: '))
+b = int(input('Segundo número: '))
+
+soma = a + b
+print(soma)
