1caad706a86d25d38e8cd718d931c45b899dc613
alanraso <alanrraso@gmail.com>
2018-04-25 21:01:10 -0300

    Corrigio imc

--- Exercicios/Aula1/imc1.py
+++ Exercicios/Aula1/imc1.py
@@ -1,13 +1,9 @@
-# 1. Faça uma que recebe o peso em (kG) e a altura (m) como entrada e calcula o MMC
-# 2. Faça um programa que pergunta altura e peso e imprime o MMC
+# 1. Faça uma que recebe o peso em (kG) e a altura (m) como entrada e calcula o IMC
+# 2. Faça um programa que pergunta altura e peso e imprime o IMC
 
-def mmc(altura, peso):
-    return  peso/(altura**2)
+altura = float(input('Digite sua altura: '))
+peso = float(input('Digite seu peso: '))
 
-def main():
-    altura = float(input('Digite sua altura: '))
-    peso = float(input('Digite seu peso: '))
+imc = peso/(altura*altura)
 
-    print('Seu mmc eh: ', mmc(altura, peso))
-
-main()
+print('Seu imc eh: ', imc)
