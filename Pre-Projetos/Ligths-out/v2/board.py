5d7bb7f61a1837b7ecea31678e44a9c9ec1c2bdd
alanraso <alanrraso@gmail.com>
2018-04-29 12:25:58 -0300

    Mais um metodo

--- Pre-Projetos/Ligths-out/v2/board.py
+++ Pre-Projetos/Ligths-out/v2/board.py
@@ -1,8 +1,12 @@
 import turtle
 import board_aux as aux
+import sys
 
 my_t = turtle.Turtle()
 aux.init(my_t)
+
+def titulo(t):
+  my_t.getscreen().title(t)
 
 def muda_cor_fundo(cor):
   my_t.getscreen().bgcolor(cor)
@@ -13,6 +17,9 @@ def desenha_circulo(pos_centro, raio, cor = '#FFFFFF'):
 
 def registrar_click(acao_click):
   my_t.getscreen().onclick(lambda x, y: acao_click((x, y)))
+  
+def input_popup(titulo, mensagem):
+  return my_t.getscreen().textinput(titulo, mensagem)
 
-def end():
+def aguarda():
   turtle.done()
--- Pre-Projetos/Ligths-out/v2/lights_out.py
+++ Pre-Projetos/Ligths-out/v2/lights_out.py
@@ -1,13 +1,20 @@
 import board
 
+MENSAGEM_INICIAL = 'O objetivo deste jogo é apagar todas as lâmpadas, ou ascender todas.\nQual seu nome?'
+
 def quando_clicka(pos):
   print(pos[0], pos[1])
+
+def desenha_tabuleiro():
   board.desenha_circulo([0, 0], 50, '#FF0000')
 
 def main():
+  board.titulo('Lights-out')
+  desenha_tabuleiro()
+  nome = board.input_popup('Bem-vindo ao Lights-out!', MENSAGEM_INICIAL)
   board.registrar_click(quando_clicka)
-  board.desenha_circulo([0, 0], 50)
-  board.end()
+  print(nome)
+  board.aguarda()
 
 if __name__ == "__main__":
   main()
