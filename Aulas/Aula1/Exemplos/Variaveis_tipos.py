a652321ced2aaa504498f7c08acada111d280fdb
alanraso <alanrraso@gmail.com>
2018-05-12 13:02:30 -0300

    Aula 1 - Exemplos

---
+++ Aulas/Aula1/Exemplos/Variaveis_tipos.py
@@ -0,0 +1,18 @@
+num = 3
+print('Variavel num: ', num)
+print(type(num))
+
+# Vou transformar num em float
+num_tipo_float = float(num)
+print('Variavel num tipo float:', num_tipo_float)
+print(type(num_tipo_float)) # imprime o tipo da variavel
+
+# Vou transformar num em string
+num_tipo_str = str(num)
+print('Variavel num tipo str:', num_tipo_str)
+print(type(num_tipo_str))
+
+# Vou transformar num em boolean
+num_tipo_bool = bool(num)
+print('Variavel num tipo bool:', num_tipo_bool)
+print(type(num_tipo_bool))
