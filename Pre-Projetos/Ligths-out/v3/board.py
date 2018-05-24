630edaf3304d7eb8ddb75aa2f796328885f2fbf6
leticiasrodrigues <leticiasilva_650@hotmail.com>
2018-05-25 00:21:18 +0200

    funçoes de base

---
+++ Pre-Projetos/Ligths-out/v3/board.py
@@ -0,0 +1,25 @@
+import turtle
+import board_aux as aux
+import sys
+
+my_t = turtle.Turtle()
+aux.init(my_t)
+
+def titulo(t):
+  my_t.getscreen().title(t)
+
+def muda_cor_fundo(cor):
+  my_t.getscreen().bgcolor(cor)
+
+def desenha_circulo(pos_centro, raio, cor = '#FFFFFF'):
+  aux.muda_pos(pos_centro)
+  my_t.dot(2 * raio, cor)
+
+def registrar_click(acao_click):
+  my_t.getscreen().onclick(acao_click)
+  
+def input_popup(titulo, mensagem):
+  return my_t.getscreen().textinput(titulo, mensagem)
+
+def aguarda():
+  turtle.done()
---
+++ Pre-Projetos/Ligths-out/v3/board_aux.py
@@ -0,0 +1,24 @@
+import turtle
+
+CINZA = '#4A4A4A'
+
+my_t = None
+
+def init(turtle_var):
+  global my_t
+  my_t = turtle_var
+  setup_inicial()
+
+def setup_inicial():
+  pular_animacoes()
+  my_t.getscreen().bgcolor(CINZA)
+  my_t.pen(my_t.pen(), shown = False) # Hide Pen
+
+def pular_animacoes():
+  my_t.speed(0)
+  my_t._tracer(0, 0)
+
+def muda_pos(pos):
+  my_t.penup()
+  my_t.setpos(pos)
+  my_t.pendown()
