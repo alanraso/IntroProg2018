2100da3f9dbafbc24319280fa30c0bdd6a7bb5f7
alanraso <alanrraso@gmail.com>
2018-06-01 19:46:26 -0300

    aula 3

---
+++ Aulas/Aula3/Exemplos/utilidade.py
@@ -0,0 +1,38 @@
+# E se eu quisesse calcular 5 vezes seguidas as bolas de sorvete o exercício passado?
+
+# Sem função
+despesa_anual = 12*1800
+
+lucro_desejado = float(input('Olá sorveteiro, digite o lucro desejado (em R$): '))
+bolas_de_sorvete = (lucro_desejado + despesa_anual)/3
+print('Você deve vender',  int(bolas_de_sorvete), 'bolas de sorvete.')
+
+lucro_desejado = float(input('Olá sorveteiro, digite o lucro desejado (em R$): '))
+bolas_de_sorvete = (lucro_desejado + despesa_anual)/3
+print('Você deve vender',  int(bolas_de_sorvete), 'bolas de sorvete.')
+
+lucro_desejado = float(input('Olá sorveteiro, digite o lucro desejado (em R$): '))
+bolas_de_sorvete = (lucro_desejado + despesa_anual)/3
+print('Você deve vender',  int(bolas_de_sorvete), 'bolas de sorvete.')
+
+lucro_desejado = float(input('Olá sorveteiro, digite o lucro desejado (em R$): '))
+bolas_de_sorvete = (lucro_desejado + despesa_anual)/3
+print('Você deve vender',  int(bolas_de_sorvete), 'bolas de sorvete.')
+
+lucro_desejado = float(input('Olá sorveteiro, digite o lucro desejado (em R$): '))
+bolas_de_sorvete = (lucro_desejado + despesa_anual)/3
+print('Você deve vender',  int(bolas_de_sorvete), 'bolas de sorvete.')
+
+# Com função
+def calcula_bolas_sorvete():
+  despesa_anual = 12*1800
+  lucro_desejado = float(input('Olá sorveteiro, digite o lucro desejado (em R$): '))
+  bolas_de_sorvete = (lucro_desejado + despesa_anual)/3
+  print('Você deve vender',  int(bolas_de_sorvete), 'bolas de sorvete.')
+
+calcula_bolas_sorvete()
+calcula_bolas_sorvete()
+calcula_bolas_sorvete()
+calcula_bolas_sorvete()
+calcula_bolas_sorvete()
+calcula_bolas_sorvete()
