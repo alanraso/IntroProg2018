65ee133fdb8b055d177eacf917fdcd1639ce45c9
alanraso <alanrraso@gmail.com>
2018-06-07 23:40:46 -0300

    Roteiro projeto Lights-out e aula 4

---
+++ Aulas/Aula4/Exemplos/exp_booleanas.py
@@ -0,0 +1,58 @@
+# Expressão numérica
+num = 3 + 5*6 + 7/(6 + 1) - 45
+
+# --------------------
+# Expressões booleanas
+# --------------------
+print('Expressões booleanas')
+comp = 3 > 1
+print(comp)
+print(type(comp))
+
+# É possível usar variáveis
+print('Usando variável')
+num = 40
+comp = num > 60
+print(comp)
+print(type(comp))
+
+# Comparadores
+print('Comparadores')
+num = 40
+comp_maior = num > 40
+comp_menor = num < 40
+comp_menor_igual = num <= 40
+comp_maior_igual = num >= 40
+comp_igual = num == 40  # Não confundir com o =
+comp_diferente = num != 40
+
+# Operadores
+
+num = 40
+
+# and  - "e" - Uma coisa e outra
+print('and - Uma coisa e outra')
+op_e = num > 20 and num < 60
+print(op_e)
+op_e = num > 50 and num < 60
+print(op_e)
+op_e = num > 20 and num < 30
+print(op_e)
+op_e = num > 50 and num < 30
+print(op_e)
+
+# or  - "ou" - Uma coisa ou outra
+print('or - Uma coisa ou outra')
+op_or = num > 20 or num < 60
+print(op_or)
+op_or = num > 50 or num < 60
+print(op_or)
+op_or = num > 20 or num < 30
+print(op_or)
+op_or = num > 50 or num < 30
+print(op_or)
+
+# not  - o oposto
+print('not - o oposto')
+print(num < 5)
+print(not num < 5)
---
+++ Aulas/Aula4/Exemplos/if-else.py
@@ -0,0 +1,21 @@
+# 1
+num = input('Quantos dias tem uma semana? ')
+
+if num == 7:
+  print('Acertou!')
+
+# 2
+num = input('Quantos dias tem uma semana? ')
+
+if num == 7:
+  print('Acertou!')
+if num != 7:
+  print('Errou!!!!')
+
+# 3
+num = input('Quantos dias tem uma semana? ')
+
+if num == 7:
+  print('Acertou!')
+else:
+  print('Errou!!!!')
