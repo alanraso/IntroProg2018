41f912a86460ee2ad03ff0c4edd49d67ef684248
alanraso <alan.raso@taqtile.com.br>
2019-12-07 09:02:25 -0300

    Exemplos de aula

---
+++ 5. Lista/Exemplos/5.criar_lista.py
@@ -0,0 +1,7 @@
+# E se eu quiser criar uma lista de 1 a 200?
+lista = []
+
+for i in range(0, 200):
+  lista.append(i + 1)
+
+print(lista)
---
+++ 5. Lista/Exemplos/6.soma_elementos.py
@@ -0,0 +1,8 @@
+# Somar os elementos de uma lista
+lista = [4, 2, 10, 25, 40] # Soma é 81
+soma = 0
+
+for num in lista:
+  soma += num
+
+print(soma)
